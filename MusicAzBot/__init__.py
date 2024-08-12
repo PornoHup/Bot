c09d1d9a9cb6ed24592c2bf5f4371b49cd4a459d
PornoHup <107538335+PornoHup@users.noreply.github.com>
2024-08-13 00:23:21 +0400

    Update __init__.py

--- MusicAzBot/__init__.py
+++ MusicAzBot/__init__.py
@@ -3,19 +3,10 @@
 # Repo Açığdısa İcazəsis Götürmə
 
 
-import logging
-from pyrogram import Client
-from MusicAzBot.config import Config
+from os.path import dirname, basename, isfile, join
+import glob
 
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-
-LOGGER = logging.getLogger(__name__)
-
-MusicAzBot = Client(
-    'MusicAzBot',
-    bot_token = Config.BOT_TOKEN,
-    api_id = Config.API_ID,
-    api_hash = Config.API_HASH
-)
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [
+    basename(f)[:-3] for f in modules if isfile(f) and not f.endswith("__init__.py")
+]
